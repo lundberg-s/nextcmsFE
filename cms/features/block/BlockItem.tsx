c18f01939cd470dbf6a56caf0feae689e8e3a2db
Sadi Lundberg <sadi.lundberg@hotmail.com>
2025-05-07 12:44:44 +0200

    refactor draggable into its own component

--- cms/features/block/BlockItem.tsx
+++ cms/features/block/BlockItem.tsx
@@ -1,5 +1,3 @@
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
 import { BlockProvider } from "@/cms/lib/providers/BlockProvider";
 import { DragHandle } from "@/cms/components/ui/drag-handle";
 import { Block } from "@/cms/lib/types/blocks";
@@ -11,24 +9,15 @@ import { EditBlockForm } from "./EditBlockForm";
 import { useSidebar } from "@/cms/components/ui/sidebar";
 import { useSidebarContent } from "@/cms/lib/context/SidebarContext";
 import { getIcon } from "@/cms/lib/utilities/GetIcon";
-import { useState } from "react";
 import { useBlock } from "@/cms/lib/hooks/useBlock";
+import { Draggable } from "@/cms/components/dnd/Draggable";
 
 export function BlockItem({ block }: { block: Block }) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id: block?.id });
   const { toggleSidebar, setOpen, open } = useSidebar();
   const { setBody, clear } = useSidebarContent();
   const { selectedBlock, setSelectedBlock } = useCmsContext();
   const { previewBlock } = useBlockPreview();
   const { removeBlock } = useBlock();
-  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const trashIcon = getIcon("trash");
   const settingsIcon = getIcon("settings");
@@ -57,60 +46,51 @@ export function BlockItem({ block }: { block: Block }) {
       setSelectedBlock(null);
       return;
     }
-
-    setIsTransitioning(true);
-
     if (selectedBlock) clear();
 
     setSelectedBlock(block);
     setBody(forms.editBlock);
 
     if (!open) toggleSidebar();
-
-    requestAnimationFrame(() => {
-      setIsTransitioning(false);
-    });
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      style={{
-        transform: CSS.Transform.toString(transform),
-        transition,
-        opacity: isDragging ? 0 : 1,
-      }}
-      className="relative group"
-    >
-      {block && (
+    <Draggable id={block.id} className="relative group">
+      {({ attributes, listeners }) => (
         <>
-          {selectedBlock?.id === block.id && !isTransitioning ? (
-            <>{previewBlock && <BlockProvider block={previewBlock} />}</>
-          ) : (
-            <BlockProvider block={block} />
+          {block && (
+            <>
+              {selectedBlock?.id === block.id ? (
+                <>{previewBlock && <BlockProvider block={previewBlock} />}</>
+              ) : (
+                <BlockProvider block={block} />
+              )}
+
+              <div className="absolute top-1/2 right-4 flex flex-col gap-10 items-center transform -translate-y-1/2">
+                <Button
+                  icon={settingsIcon}
+                  variant="ghost"
+                  size="sm"
+                  onClick={handleClick}
+                />
+
+                <DragHandle attributes={attributes} listeners={listeners} />
+
+                <ConfirmationModal
+                  trigger={
+                    <Button icon={trashIcon} variant="ghost" size="sm" />
+                  }
+                  title="Delete Block"
+                  description="Are you sure you want to delete this block? This action cannot be undone."
+                  onConfirm={() => removeBlock(block.id)}
+                  cancelText="Cancel"
+                  confirmText="Delete"
+                />
+              </div>
+            </>
           )}
-
-          <div className="absolute top-1/2 right-4 flex flex-col gap-10 items-center transform -translate-y-1/2">
-            <Button
-              icon={settingsIcon}
-              variant="ghost"
-              size="sm"
-              onClick={handleClick}
-            />
-
-            <DragHandle attributes={attributes} listeners={listeners} />
-
-            <ConfirmationModal
-              trigger={<Button icon={trashIcon} variant="ghost" size="sm" />}
-              title="Delete Block"
-              description="Are you sure you want to delete this block? This action cannot be undone."
-              onConfirm={() => removeBlock(block.id)}
-              cancelText="Cancel"
-              confirmText="Delete"
-            />
-          </div>
         </>
       )}
-    </div>
+    </Draggable>
   );
 }
