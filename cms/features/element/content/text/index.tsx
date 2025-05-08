d9307bf8e9c827e58b1c2a0848272719ed55da97
Sadi Lundberg <sadi.lundberg@hotmail.com>
2025-05-08 18:25:18 +0200

    feat: implement Edit, Render, and Preview components for text content management

---
+++ cms/features/element/content/text/index.tsx
@@ -0,0 +1,7 @@
+import { EditText } from "./Edit";
+import { RenderText } from "./Render";
+import { PreviewText } from "./Preview";
+
+export const Edit = { Text: EditText };
+export const Render = { Text: RenderText };
+export const Preview = { Text: PreviewText };
